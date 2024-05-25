7d36adb0434da7a79f659d2b17743868913eaf25
Armx <114233771+DarkenedSky-123@users.noreply.github.com>
2024-05-25 11:03:52 +0530

    Update shorts_main.py

--- shorts_main.py
+++ shorts_main.py
@@ -5,6 +5,7 @@ print(os.environ['API'])
 
 try:
         for i in range(0,0, 1):
+                subprocess.run(["python", "Python Files/create.py"], check=True)
                 subprocess.run(["python", "Python Files/files.py"], check=True)
                 subprocess.run(["python", "Python Files/daata/read_dc.py"], check=True)
                 subprocess.run(["python", "Python Files/inro.py"], check=True)
