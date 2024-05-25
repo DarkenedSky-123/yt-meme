ca12c9b0484c7c8a33f0c234c02aeea5b2979d8d
Armx <114233771+DarkenedSky-123@users.noreply.github.com>
2024-05-25 15:54:37 +0530

    Update shorts_main.py

--- shorts_main.py
+++ shorts_main.py
@@ -4,19 +4,9 @@ import os
 
 
 try:
-        for i in range(0,0, 1):
-                subprocess.run(["python", "Python Files/ff.py"], check=True)
-                subprocess.run(["python", "Python Files/create.py"], check=True)
-                subprocess.run(["python", "Python Files/files.py"], check=True)
-                subprocess.run(["python", "Python Files/daata/read_dc.py"], check=True)
-                subprocess.run(["python", "Python Files/inro.py"], check=True)
-                subprocess.run(["python", "Python Files/outro.py"], check=True)
-                subprocess.run(["python", "Python Files/download.py"], check=True)
-                subprocess.run(["python", "Python Files/fps.py"], check=True)
-                subprocess.run(["python", "Python Files/join.py"], check=True)
-                subprocess.run(["python", "Python Files/delete.py"], check=True)
-                subprocess.run(["python", "Python Files/upload.py"], check=True)
-                subprocess.run(["python", "Python Files/daata/write_dc.py"], check=True)
+        for i in range(0,1, 1):
+                subprocess.run(["python", "Python Files/Discord/main.py"], check=True)
+
 
                 print("##########################################################")
                 print(f"###################### video {i+1} ###########################")
