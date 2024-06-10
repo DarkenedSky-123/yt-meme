a05559cdd1bbf5f894942d9a473b5760885242f1
Armx <114233771+DarkenedSky-123@users.noreply.github.com>
2024-06-10 17:34:21 +0530

    Update shorts_main.py

--- shorts_main.py
+++ shorts_main.py
@@ -1,39 +1,29 @@
-import praw
+import subprocess
+import json
+import os
 
-username = "Armx11443"
-password = "Armxisaboss123"
-client_id = "PX5291z-uAykEUfeaHlGRQ"
-client_secret = "oCTu3AvbxtMC7kyg9zqW6VVzsQlLHQ"
 
-reddit_instance = praw.Reddit(
-    client_id=client_id,
-    client_secret=client_secret,
-    username=username,
-    password=password,
-    user_agent="armx"
-)
+try:
+        for i in range(0,1, 1):
+                # subprocess.run(["python", "Python Files/Discord/main.py"], check=True)
+                # subprocess.run(["python", "Python Files/create.py"], check=True)
+                # subprocess.run(["python", "Python Files/files.py"], check=True)
+                # subprocess.run(["python", "Python Files/daata/read_dc.py"], check=True)
+                # subprocess.run(["python", "Python Files/inro.py"], check=True)
+                # subprocess.run(["python", "Python Files/outro.py"], check=True)
+                subprocess.run(["python", "Python Files/download.py"], check=True)
+                # subprocess.run(["python", "Python Files/fps.py"], check=True)
+                # subprocess.run(["python", "Python Files/join.py"], check=True)
+                # subprocess.run(["python", "Python Files/delete.py"], check=True)
+                # subprocess.run(["python", "Python Files/upload.py"], check=True)
+                # subprocess.run(["python", "Python Files/daata/write_dc.py"], check=True)
 
-print(reddit_instance.user.me())
+                print("##########################################################")
+                print(f"###################### video {i+1} ###########################")
+                print("##########################################################")
 
-choice = reddit_instance.subreddit("MemeVideos")
-new_messages = choice.new(limit=10)
-
-for message in new_messages:
-    print("Post permalink:", "https://reddit.com"+message.permalink)
-    # print("Post URL:", message.url)
-    
-    if hasattr(message, 'media') and message.media is not None:
-        media_info = message.media
-        if 'reddit_video' in media_info:
-            reddit_video = media_info['reddit_video']
-            video_url = reddit_video.get('fallback_url')
-            # Uncomment the lines below if needed for DASH quality and audio links
-            print("Video URL:"+video_url)
-            print("Audio URL:"+message.url + "/DASH_AUDIO_128.mp4")
-            print("Final Link:" +f"https://sd.rapidsave.com/download.php?permalink={"https://reddit.com"+message.permalink}&video_url={video_url}&audio_url={message.url + "/DASH_AUDIO_128.mp4"}")
-            print("#####################################################################################################################################")
-
-    else:
-        print("No video media found.")
-    
-#https://sd.rapidsave.com/download.php?permalink=https://reddit.com/r/MemeVideos/comments/1db6zwe/yeah_its_just_an_anime_its_cant_hurt_you/&video_url=https://v.redd.it/ovkaaxmhld5d1/DASH_720.mp4?source=fallback&audio_url=https://v.redd.it/ovkaaxmhld5d1/DASH_AUDIO_128.mp4
+except json.decoder.JSONDecodeError as e:
+        print(f"JSONDecodeError: {e}")
+# subprocess.run(["python", "Python Files/message.py"], check=True)
+# https://www.youtube.com/shorts/4lixIz4Hw9c
+# https://youtube.com/shorts/FNkEB9_wAqU?si=4SM4WV3bEWTigKqQ
