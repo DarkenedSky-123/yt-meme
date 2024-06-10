098dacc7ee22405cba9fd356de8f7e7b8b556dee
Armx <114233771+DarkenedSky-123@users.noreply.github.com>
2024-06-10 07:26:30 +0530

    Update shorts_main.py

--- shorts_main.py
+++ shorts_main.py
@@ -19,18 +19,21 @@ choice = reddit_instance.subreddit("MemeVideos")
 new_messages = choice.new(limit=10)
 
 for message in new_messages:
-    print("Post permalink:", message.permalink)
-    print("Post URL:", message.url)
+    print("Post permalink:", "https://reddit.com"+message.permalink)
+    # print("Post URL:", message.url)
     
     if hasattr(message, 'media') and message.media is not None:
         media_info = message.media
         if 'reddit_video' in media_info:
             reddit_video = media_info['reddit_video']
-            video_width = reddit_video.get('fallback_url')
-            print("Video Width:", video_width)
+            video_url = reddit_video.get('fallback_url')
             # Uncomment the lines below if needed for DASH quality and audio links
-            # quality = f"/DASH_{video_width}.mp4"
-            # print(message.url + quality)
-            # print(message.url + "/DASH_AUDIO_128.mp4")
+            print("Video URL:"+video_url)
+            print("Audio URL:"+message.url + "/DASH_AUDIO_128.mp4")
+            print("Final Link:" +f"https://sd.rapidsave.com/download.php?permalink={"https://reddit.com"+message.permalink}&video_url={video_url}&audio_url={message.url + "/DASH_AUDIO_128.mp4"}")
+            print("#####################################################################################################################################")
+
     else:
         print("No video media found.")
+    
+#https://sd.rapidsave.com/download.php?permalink=https://reddit.com/r/MemeVideos/comments/1db6zwe/yeah_its_just_an_anime_its_cant_hurt_you/&video_url=https://v.redd.it/ovkaaxmhld5d1/DASH_720.mp4?source=fallback&audio_url=https://v.redd.it/ovkaaxmhld5d1/DASH_AUDIO_128.mp4
