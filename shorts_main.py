6493caf87b39cec6521970b25a5465a431c21894
Armx <114233771+DarkenedSky-123@users.noreply.github.com>
2024-06-10 17:49:56 +0530

    Update shorts_main.py

--- shorts_main.py
+++ shorts_main.py
@@ -6,7 +6,7 @@ import os
 try:
         for i in range(0,1, 1):
                 # subprocess.run(["python", "Python Files/Discord/main.py"], check=True)
-                # subprocess.run(["python", "Python Files/create.py"], check=True)
+                subprocess.run(["python", "Python Files/create.py"], check=True)
                 # subprocess.run(["python", "Python Files/files.py"], check=True)
                 # subprocess.run(["python", "Python Files/daata/read_dc.py"], check=True)
                 # subprocess.run(["python", "Python Files/inro.py"], check=True)
